187659a9ff2540fc2d1375fc6317488f645d0539
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2021-09-13 10:51:54 +0200

    Kiinduló állapot (Staged)

--- Jackie/Program.cs
+++ Jackie/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Jackie
 {
@@ -10,6 +11,44 @@ namespace Jackie
   {
     static void Main(string[] args)
     {
+      elsofeladat();
+      masodikfeladat();
+      harmdadikfeladat();
+      negyedikfeladat();
+      otodikfeladat();
+      hatodikfeladat();
+
+      Console.ReadKey();
+    }
+
+    private static void hatodikfeladat()
+    {
+     
+    }
+
+    private static void otodikfeladat()
+    {
+      
+    }
+
+    private static void negyedikfeladat()
+    {
+      
+    }
+
+    private static void harmdadikfeladat()
+    {
+     
+    }
+
+    private static void masodikfeladat()
+    {
+      
+    }
+
+    private static void elsofeladat()
+    {
+     
     }
   }
 }
